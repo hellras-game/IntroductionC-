c0ae77f6c20c9fb47094c6dfd6a64cdcf3c36c61
hellras-game <72730151+hellras-game@users.noreply.github.com>
2021-10-12 15:41:51 +1100

    Add files via upload

---
+++ Task5-1/Task5-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл
+            List<string> messList = new List<string>();
+            string s = "first";
+            while (s != "")
+            {
+                string curr = Console.ReadLine();
+                s = curr;
+                if (s == "")
+                    break;
+                messList.Add(curr);
+            }
+
+            File.WriteAllLines("message.txt", messList);
+        }
+    }
+}
