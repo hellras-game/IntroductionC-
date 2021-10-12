0a5a6b657c3b7afd164d77a2394078f10c081ba4
hellras-game <72730151+hellras-game@users.noreply.github.com>
2021-10-12 16:52:02 +1100

    Add files via upload

---
+++ Task2-1/Task2-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+            Console.WriteLine("Введите минимальную и максимальную температуру через пробел.");
+            string s = Convert.ToString(Console.ReadLine());
+            string[] t = s.Split(' ');
+            float min = float.Parse(t[0]);
+            float max = float.Parse(t[1]);
+            float val = (min + max) / 2;
+            Console.WriteLine("Среднесуточная температура равна {0}", val);
+        }
+    }
+}
