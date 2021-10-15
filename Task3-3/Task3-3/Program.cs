8ee22d47420ea6207a66cc45c5109d1946b88e56
hellras-game <72730151+hellras-game@users.noreply.github.com>
2021-10-15 20:47:01 +1100

    Add files via upload

---
+++ Task3-3/Task3-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, выводящую введённую пользователем строку в обратном порядке (olleH вместо Hello).
+            Console.WriteLine("Введите строку:");
+            string s = Convert.ToString(Console.ReadLine());
+            char[] inv = new char[s.Length];
+            int j = s.Length - 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                inv[i] = s[j];
+                j--;
+            }
+            s = new string(inv);
+            Console.WriteLine(s);
+        }
+    }
+}
